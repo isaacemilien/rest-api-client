558c7bb005a7bfa70bd1365688c8d46d3b7cc287
isaacemilien <isaac.emilien@gmail.com>
2024-02-27 15:25:51 +0000

    Added initial call to API

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-console.log("lskdjfslkdfj");
+window.addEventListener("load", () => {
+  const btn1 = document.querySelector(".search-form");
+  const input = document.querySelector("#search");
 
-// Define the API URL
-const apiUrl = 'https://api.vam.ac.uk/v2/museumobject/O828146';
+  btn1.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-// Make a GET request
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
+    console.log(input.value);
+
+    const search = input.value;
+
+    // construct url
+    const url = "https://api.vam.ac.uk/v2/objects/search?q=" + encodeURIComponent(search);
+
+    var xhr = new XMLHttpRequest();
+
+
+    xhr.addEventListener("load", () => {
+      if (xhr.status == 200) {
+        const data = JSON.parse(xhr.responseText);
+
+        const contentSection = document.querySelector("#search-results");
+
+
+        for (record of data.records) {
+
+          console.log(record);
+
+          // console.log(item.data[0].title);
+          // console.log(item.data[0].description);
+          // console.log(item.links[0].href);
+
+          const img = document.createElement('img');
+          img.setAttribute("src", record._images._primary_thumbnail);
+
+
+          contentSection.appendChild(img);
+        }
+      } else {
+        console.log(xhr.status)
+      }
+    });
+
+    xhr.open("GET", url, true);
+    xhr.send();
   })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+});
