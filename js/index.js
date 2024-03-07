525c06664c1d23923d739f899b956fc519a280f7
Isaac <isaac.emilien@gmail.com>
2024-03-07 00:03:42 +0000

    Restructured files into folders, added basic validation

--- js/index.js
+++ js/index.js
@@ -2,32 +2,44 @@ window.addEventListener("load", () => {
   const btn1 = document.querySelector(".search-form");
   const input = document.querySelector("#search");
   const loadingRing = document.querySelector(".lds-dual-ring");
+  const contentSection = document.querySelector("#search-results");
+  const userAlert = document.querySelector("#alert-content")
 
   btn1.addEventListener("submit", async (event) => {
     event.preventDefault();
-    loadingRing.classList.remove("hidden");
 
-    const search = input.value;
-    const url = "https://api.vam.ac.uk/v2/objects/search?&q=" + encodeURIComponent(search);
+    const searchInput = input.value;
+    const url = "https://api.vam.ac.uk/v2/objects/search?&q=" + encodeURIComponent(searchInput);
+
+    if (!searchInput) {
+      userAlert.classList.remove("hidden");
+      userAlert.firstElementChild.textContent = "You cannot leave the search input box empty.";
+      return;
+    }
+
+    userAlert.classList.add("hidden");
+
 
     try {
+      // Clear previous search results
+      while (contentSection.firstChild) {
+        contentSection.removeChild(contentSection.lastChild);
+      }
+
+      loadingRing.classList.remove("hidden");
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data = await response.json();
 
-      const contentSection = document.querySelector("#search-results");
-
-      // Clear previous search results
-      while (contentSection.firstChild) {
-        contentSection.removeChild(contentSection.lastChild);
+      if (Object.keys(data.records).length == 0) {
+        userAlert.classList.remove("hidden");
+        userAlert.firstElementChild.textContent = "Oops! We couldn't find any results matching your search query.";
       }
 
       // Iterate through records and create elements
       for (let record of data.records) {
-        console.log(record);
-
         const contentCard = document.createElement('div');
         const figureCaption = document.createElement('figcaption');
         const title = document.createElement('div');
